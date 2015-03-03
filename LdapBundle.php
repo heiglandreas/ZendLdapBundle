97d1b70185a36d4f818aa500a902ef16f847f048
Andreas Heigl <andreas@heigl.org>
2015-03-03 11:02:50 +0100

    Renamed Bundle-file

--- LdapBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Org_Heigl\LdapBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LdapBundle extends Bundle
-{
-}
---
+++ ZendLdapBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Org_Heigl\Bundle\ZendLdapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZendLdapBundle extends Bundle
+{
+}
